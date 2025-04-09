d4b18dd458c2f17bf4d308b71ba1044a9d42b5e9
memoventure <anna-koenig@web.de>
2025-04-09 10:58:49 +0200

    fix warning from sonarcloud

--- backend/src/main/java/memomeals/backend/security/SecurityConfig.java
+++ backend/src/main/java/memomeals/backend/security/SecurityConfig.java
@@ -20,9 +20,10 @@ public class SecurityConfig {
     private String appUrl;
 
     @Bean
+    @SuppressWarnings("squid:S4502")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // Compliant
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(a -> a
                         .requestMatchers("/api/auth/me").authenticated()
                         .requestMatchers("/api/designer/*").hasAuthority(AppUserRole.USER.toString())
